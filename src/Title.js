6b0e3837989030989d2e12c849ca584d709e9511
Yusuke-Takakura <p8g43blf@s.okayama-u.ac.jp>
2020-09-19 16:53:50 +0900

    todoapp作るよ

---
+++ src/Title.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Title = ({ name }) => {
+    return (
+        <div>
+            <h1>Hello World!</h1>
+            <h2>ようこそ{name}さん</h2>
+        </div>
+    )
+}
+
+export default Title;
